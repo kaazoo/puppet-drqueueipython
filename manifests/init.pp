9c7007407d061d4935a4978378fa31936adfc75a
Andreas Schroeder <andreas@drqueue.org>
2013-11-27 00:08:59 +0100

    make mongodb ip address configurable

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,3 @@
-class drqueueipython ($role='slave', $master='127.0.0.1', $public_key='', $install_method='git', $drqueue_version='1.0', $git_branch='master', $git_tag='', $python_version = '2.7') {
+class drqueueipython ($role='slave', $master='127.0.0.1', $mongodb='127.0.0.1', $public_key='', $install_method='git', $drqueue_version='1.0', $git_branch='master', $git_tag='', $python_version = '2.7') {
   include drqueueipython::config, drqueueipython::install
 }
